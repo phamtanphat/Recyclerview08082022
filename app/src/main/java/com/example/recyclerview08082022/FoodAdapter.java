0a36963f75869e19a45695b06dd1c50bba4ae5ee
Phat <phatdroid94@gmail.com>
2022-09-28 20:03:52 +0700

    update view

--- app/src/main/java/com/example/recyclerview08082022/FoodAdapter.java
+++ app/src/main/java/com/example/recyclerview08082022/FoodAdapter.java
@@ -109,8 +109,8 @@ public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.FoodViewHolder
             if (!checkCurrentTimeOver(timeOpen, timeCurrent, timeClose)) {
                 tvCloseTime.setVisibility(View.GONE);
             } else {
-                String minutes = food.getMinuteOpenTime() < 9 ? "0" + food.getMinuteOpenTime() : food.getMinuteOpenTime() + "";
-                String hour = food.getHourOpenTime() < 9 ? "0" + food.getHourOpenTime() : food.getHourOpenTime() + "";
+                String minutes = food.getMinuteOpenTime() < 10 ? "0" + food.getMinuteOpenTime() : food.getMinuteOpenTime() + "";
+                String hour = food.getHourOpenTime() < 10 ? "0" + food.getHourOpenTime() : food.getHourOpenTime() + "";
                 tvCloseTime.setText(String.format("Đóng cửa \n Đặt bàn vào lúc \n%s:%s", hour, minutes));
                 tvCloseTime.setVisibility(View.VISIBLE);
             }
